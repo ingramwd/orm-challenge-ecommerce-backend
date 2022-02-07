28db3ff681471c96693992e53abcaf011dcaeb14
ingramwd <Ingramwd01@gmail.com>
2022-02-06 19:50:28 -0500

    added tag findAll route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,30 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id']
+      }
+    ]
+  })
+    .then(dbTagData => {
+      if (dbTagData) {
+        res.json(dbTagData)
+      } else (err) => {
+        res.status(500).json(err)
+      }
+    })
 });
 
 router.get('/:id', (req, res) => {
